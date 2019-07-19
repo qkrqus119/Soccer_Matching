07516eb67ec1444456ea4cc153f9bf021bd6ef30
thinklim <sagalim3983@gmail.com>
2019-07-19 14:43:16 +0900

    addProject

---
+++ Soccer_Matching/src/main/java/com/soccermatching/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.soccermatching.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String getHello(Model model) {
+		return "hello";
+	}
+}
