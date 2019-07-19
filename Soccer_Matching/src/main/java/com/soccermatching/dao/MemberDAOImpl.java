435e944d18a5bcabac603e7c6b8a435bf1b12576
thinklim <sagalim3983@gmail.com>
2019-07-19 16:32:45 +0900

    addMemberAPI

--- Soccer_Matching/src/main/java/com/soccermatching/dao/MemberDAOImpl.java
+++ Soccer_Matching/src/main/java/com/soccermatching/dao/MemberDAOImpl.java
@@ -13,7 +13,7 @@ import com.soccermatching.dto.MemberDTO;
 
 @Repository
 public class MemberDAOImpl implements MemberDAO {
-	
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
@@ -21,26 +21,46 @@ public class MemberDAOImpl implements MemberDAO {
 	public List<MemberDTO> readAll() {
 		return jdbcTemplate.query("select * from member", new MemberDTOMapper());
 	}
-	
+
 	@Override
 	public MemberDTO read(int number) {
 		return jdbcTemplate.queryForObject("select * from member where number = ?", new MemberDTOMapper(), number);
 	}
-	
+
+	@Override
+	public void create(MemberDTO memberDTO) {
+		jdbcTemplate.update(
+				"insert into member (id, pwd, name, gender, cphone, birthday, email) values (?, ?, ?, ?, ?, ?, ?)",
+				memberDTO.getId(), memberDTO.getPwd(), memberDTO.getName(), memberDTO.getGender(),
+				memberDTO.getCphone(), memberDTO.getBirthday(), memberDTO.getEmail());
+	}
+
+	@Override
+	public void update(String pwd, String name, int gender, String cphone, String birthday, String email, int number) {
+		jdbcTemplate.update(
+				"update member set pwd = ?, name = ?, gender = ?, cphone = ?, birthday = ?, email = ? where number = ?",
+				pwd, name, gender, cphone, birthday, email, number);
+	}
+
+	@Override
+	public void delete(int number) {
+		jdbcTemplate.update("delete from member where number = ?", number);
+	}
+
 	public final class MemberDTOMapper implements RowMapper<MemberDTO> {
-	    public MemberDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
-	        MemberDTO memberDTO = new MemberDTO();
-	        memberDTO.setBirthday(rs.getString("birthday"));
-	        memberDTO.setCphone(rs.getString("cphone"));
-	        memberDTO.setEmail(rs.getString("email"));
-	        memberDTO.setGender(rs.getInt("gender"));
-	        memberDTO.setId(rs.getString("id"));
-	        memberDTO.setName(rs.getString("name"));
-	        memberDTO.setNumber(rs.getInt("number"));
-	        memberDTO.setPwd(rs.getString("pwd"));
-	        
+		public MemberDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+			MemberDTO memberDTO = new MemberDTO();
+			memberDTO.setBirthday(rs.getString("birthday"));
+			memberDTO.setCphone(rs.getString("cphone"));
+			memberDTO.setEmail(rs.getString("email"));
+			memberDTO.setGender(rs.getInt("gender"));
+			memberDTO.setId(rs.getString("id"));
+			memberDTO.setName(rs.getString("name"));
+			memberDTO.setNumber(rs.getInt("number"));
+			memberDTO.setPwd(rs.getString("pwd"));
+
 			return memberDTO;
-	    }
+		}
 	}
 
 }
